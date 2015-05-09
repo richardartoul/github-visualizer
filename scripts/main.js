8fc09dcc9c6db71731ed83710e305f0d32464098
Richard Artoul <richardartoul@gmail.com>
2015-05-09 12:27:37 -0700

    Circles now display properly - functioning MVP

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,32 @@
 // var convertResultsToJSON = function(data) {
-// 	var resultsJSONString = 
-// 		"{ \n \"search\": \"" + searchText + "\", \n \"children\": [";
-// 		for (var i = 0; i < data.items; i++) {
-// 			resultsJSONString
-// 		} 
+// 	console.log(data.items);
+// 	var resultsJSONString = "{ \n \"search\": \"" + searchText + "\", \n \"children\": [";
+// 	for (var i = 0; i < data.items; i++) {
+// 		var repo = data.items[i];
+// 		console.log(repo);
+// 		resultsJSONString += "{\n \"name\": \"" + repo.name + "\", \n \"stars\": "
+// 			+ repo["stargazers count"] + " \n }"
+// 	} 
+// 	resultsJSONString += "]}";
+// 	console.log(resultsJSONString);
+// 	return JSON.parse(resultsJSONString);
 // }
+
+var convertResultsToJSON = function(data) {
+	// console.log(data.items);
+	var resultsJSONString = "{ \n \"search\": \"" + searchText + "\", \n \"children\":"
+	resultsJSONString += JSON.stringify(data.items);
+	resultsJSONString += "}";
+	// console.log(resultsJSONString);
+	return JSON.parse(resultsJSONString);
+}
 
 //gets repository results from github api when user searches
 $(document).ready(function () {
 	$('#searchBox').bind('submit',function(event) {
   		event.preventDefault();
   		var searchText = $("#searchText").val();
-  		console.log(searchText);
+  		// console.log(searchText);
   		$.ajax({
 			url: "https://api.github.com/search/repositories",
 			data: {
@@ -24,27 +39,46 @@ $(document).ready(function () {
 			success: function(data) {
 				//draws circles from ajax response data
 				searchResults = data.items;
-				console.dir(searchResults);
-				var width = 8000, height = 800;
+				// var JSONResults = JSON.stringify(data.items);
+				var JSONResults = convertResultsToJSON(data);
+				// console.log(convertResultsToJSON(data));
+				// console.log(JSONResults);
+
+				var width = 1600, height = 800;
+				var pack = d3.layout.pack()
+					.size([width,height])
+					.value(function (d) {
+						return d.stargazers_count/100;
+					})
+				var packCalculations = pack.nodes(JSONResults);
+				// console.log(packCalculations);
+				// packCalculations = pack.nodes(JSON.parse(JSONResults));
+				console.dir(packCalculations);
+				// console.dir(packCalculations);
+				// console.log(convertResultsToJSON(data));
+				// console.dir(searchResults);
 				var svg = d3.select("body").append("svg")
 					.attr("width", width)
 					.attr("height", height);
 				var bubbles = svg.selectAll("g")
-					.data(searchResults)
+					.data(packCalculations[0].children)
 					.enter()
 					.append("g")
-					.attr("transform", function(d,i) {
-						var x = 0;
-						for (var count = 0; count<i; count++) {
-							x += d.stargazers_count;
-						}
-						return "translate(" + x/100 + "," + height/7 + ")"
-					});
+					// .attr("transform", function(d,i) {
+					// 	var x = 0;
+					// 	for (var count = 0; count<i; count++) {
+					// 		x += d.stargazers_count;
+					// 	}
+					// 	return "translate(" + x/100 + "," + height/7 + ")"
+					// });
 				bubbles.append("circle")
 					.attr("r", function(d) {
 						return d.stargazers_count/100
 					})
-					.style("fill", "blue");
+					.style("fill", "blue")
+					.attr("transform", function(d,i) {
+						return "translate(" + d.x + "," + d.y + ")"
+					});
 			}
 		});
     });
